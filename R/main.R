a689b00cf60b5fd7544bc8db86ae812072c710f3
Marc Thibault <marc.thibault@jalgos.com>
2017-03-22 17:20:39 +0100

    cleaning code

--- R/main.R
+++ R/main.R
@@ -8,54 +8,44 @@ library(fGarch)
 
 source("importer.R")
 source("models.R")
+source("evaluate.R")
 
 temperature <- import.global()
-temperature <- temperature[is.na(temperature[, MAX_AIR_TEMP]) == FALSE]
 
 
-# Simuler série temp, test
-serietemp <- ts(data = rt(250, df = 10), start = 1, frequency = 1)
-plot(serietemp, ylim = c(-5,5), main ="Réalisations de variables aléatoires iid de loi de Student a 10 degres de liberté")
+# Simuler sï¿½rie temp, test
+# serietemp <- ts(data = rt(250, df = 10), start = 1, frequency = 1)
+# plot(serietemp, ylim = c(-5,5), main ="Rï¿½alisations de variables alï¿½atoires iid de loi de Student a 10 degres de libertï¿½")
 
 # Simulation de l'ARIMA
-SIMU_ARIMA <- arima.sim(model = list(0,0,0), n = 50,innov = rnorm(n), n.start = NA)
-plot(SIMU_ARIMA)
+# SIMU_ARIMA <- arima.sim(model = list(0,0,0), n = 50,innov = rnorm(n), n.start = NA)
+# plot(SIMU_ARIMA)
 
 # Ploter la serie temporelle
-ggplot(temperature[], aes(x = date, y = MAX_AIR_TEMP)) + geom_line()
+# ggplot(temperature[], aes(x = date, y = MAX_AIR_TEMP)) + geom_line()
 
 
-# On va travailler sur la série teporelle des températures
-x <- temperature[,MAX_AIR_TEMP]
-date <- temperature[,X]
+# On va travailler sur la sï¿½rie teporelle des tempï¿½ratures
+x <- temperature[, MAX_AIR_TEMP]
+date <- temperature[, X]
 
 
-# appliquer une MA
-a <- filter(x, filter=c(1,1), method = c("convolution"), sides = 2, circular = FALSE)
+# Residus de Buys Ballo
+plot.series(x, model.BB(date, x))
+evaluate.acf(model.BB(date, x)[[3]])
 
 
+# Residus de STL
+plot.series(x, model.stl(date, x))
+evaluate.acf(model.stl(date, x)[[3]])
 
-# On applique le modèle de l'AR, et on va ploter l'ACF et la PACF de l'innovation
-innovation <- model_AR(20)
-acf(innovation, lag.max = NULL,type = c("correlation", "covariance", "partial"), plot = TRUE, na.action = na.fail, demean = TRUE)
-pacf(innovation, lag.max = NULL,plot = TRUE)
+# Residus de AR
+plot.series(x, model.AR(date, x))
+evaluate.acf(model.AR(date, x)[[3]])
 
-
-
-# Méthode de Buys-Ballot
-datesquare <- date*date
-b <- lm(x ~ sin(date*2*pi/12.)+sin(2*date*2*pi/12.)+cos(2*date*2*pi/12.)+ cos(date*2*pi/12.) + date + datesquare)
-plot(x)
-lines(b$fitted.values)
-
-# Afficher la trend
-plot(b$coefficients["(Intercept)"]+b$coefficients["date"]*date+b$coefficients["datesquare"]*datesquare)
-
-# ACF et PACF pour les résidus de Buys Ballot
-acf(b$residuals, lag.max = NULL,type = c("correlation", "covariance", "partial"), plot = TRUE, na.action = na.fail, demean = TRUE)
-pacf(b$residuals, lag.max = NULL, plot = TRUE)
-plot(date*b$coefficients["date"]+datesquare*b$coefficients["datesquare"])
-
+# Residus de ARIMA
+plot.series(x, model.ARIMA(date, x))
+evaluate.acf(model.ARIMA(date, x)[[3]])
 
 # appliquer une ARIMA
 order_pdq <- c(10,3,3)
@@ -68,22 +58,3 @@ arima_estimated <- arima(x, order = order_pdq,
 
 acf(arima_estimated$residuals, lag.max = NULL,type = c("correlation", "covariance", "partial"), plot = TRUE, na.action = na.fail, demean = TRUE)
 pacf(arima_estimated$residuals, lag.max = NULL,plot = TRUE)
-
-
-
-
-stl(x, s.window = "periodic", s.degree = 0, t.window = NULL, t.degree = 1, l.window = nextodd(period), l.degree = t.degree, s.jump = ceiling(s.window/10), t.jump = ceiling(t.window/10),l.jump = ceiling(l.window/10), robust = FALSE)
-
-
-
-
-decomposition <- decompose(x, type = c("multiplicative"),filter = NULL)
-
-
-
-
-
-
-
-
-
--- R/models.R
+++ R/models.R
@@ -6,21 +6,61 @@ library(data.table)
 library(stats)
 
 
-model_AR<- function(n) {
-  # appliquer un AR
+model.AR<- function(date, x) {
   ar_estimated <- ar(x, aic = TRUE, order.max = n, method=c("yule-walker", "burg", "ols", "mle", "yw"))
-  
-  innovation <- x
-  
-  for (i in 1:n) {
-    a <- x[(i+1):length(x)]
-    for (j in 1:i) {
-      a[[length(a) + 1]] = 0
-    }
-    
-    innovation <- innovation - ar_estimated$ar[i]*a
-  }
-  return(innovation)
+
+  residuals <- ar_estimated$resid
+  residuals <- residuals[is.na(residuals) == FALSE]
+  res <- list()
+  res[[3]] <- residuals
+  res
+}
+
+model.BB <- function(date, x)
+{
+  datesquare <- date * date
+  reg <- lm(x ~ sin(date * 2 * pi / 12) + sin(2 * date * 2 * pi / 12.) + cos(2 * date * 2 * pi / 12.)+ cos(date * 2 * pi / 12.) + date + datesquare)
+
+  res <- list()
+  res[[1]] <- reg$coefficients["sin(date * 2 * pi/12)"] * sin(date * 2 * pi / 12) +
+                   reg$coefficients["sin(2 * date * 2 * pi/12)"] * sin(2 * date * 2 * pi/12) +
+                   reg$coefficients["cos(2 * date * 2 * pi/12)"] * cos(2 * date * 2 * pi/12) +
+                   reg$coefficients["cos(date * 2 * pi/12)"] * cos(date * 2 * pi / 12)
+  res[[2]] <- reg$coefficients["(Intercept)"]+reg$coefficients["date"]*date+reg$coefficients["datesquare"]*datesquare
+  res[[3]] <- c(reg$residuals)
+  res
 }
 
 
+model.stl <- function(date, x)
+{
+  a <- stl(ts(x, frequency = 12), s.window = "periodic")
+  res <- list()
+  res[[1]] <- a$time.series[, 1]
+  res[[2]] <- a$time.series[, 2]
+  res[[3]] <- a$time.series[, 3]
+  res
+}
+
+
+model.ARIMA <- function(date, x, order_pdq = c(10,3,3))
+{
+
+  arima_estimated <- arima(x, order = order_pdq,
+    seasonal = list(order = c(0, 0, 0), period = NA),
+    xreg = NULL, include.mean = TRUE,
+    transform.pars = TRUE,
+    fixed = NULL, init = NULL,
+    method = c("CSS-ML", "ML", "CSS"))
+
+  residuals <- arima_estimated$resid
+  residuals <- residuals[is.na(residuals) == FALSE]
+  res <- list()
+  res[[3]] <- residuals
+  res
+
+}
+
+
+
+# a <- model.stl(date, x)
